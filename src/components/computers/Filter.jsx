0df45144f866ed52619c2c3b87fec815155a08cc
Dhia Ben Hammouda <dhiabenhamouda.5@gmail.com>
2022-06-15 17:56:24 +0200

    more work

--- src/components/computers/Filter.jsx
+++ src/components/computers/Filter.jsx
@@ -23,9 +23,24 @@ const Filter = ({loading,setLoading,setComputers,priceRange,setPriceRange,comput
     console.log("checkbox");
   }
 
-  function procesorHandler()
+  function procesorHandler(e)
   {
     
+  }
+
+  function memoryHandler(e)
+  {
+    
+  }
+
+  function driveHandler(e)
+  {
+
+  }
+
+  function graphicsCardHandler()
+  {
+
   }
 
   return (
@@ -43,52 +58,107 @@ const Filter = ({loading,setLoading,setComputers,priceRange,setPriceRange,comput
             value={priceRange}
             valueLabelDisplay="auto"
             onChange={(e, newValue) => { setPriceRange(newValue) }}
-            onChangeCommitted={(e, newValue) => { filterData() }}
+            onChangeCommitted={filterData}
           />
           <div className="price-inputs">
-            <input className="min" value={priceRange[0] + "  DT"} onChange={() => { }} />
-            <input className="max" value={priceRange[1] + "  DT"} onChange={() => { }} />
+            <input 
+              className="min" 
+              value={priceRange[0] + "  DT"} 
+              onChange={() => { }} 
+            />
+            <input 
+              className="max" 
+              value={priceRange[1] + "  DT"} 
+              onChange={() => { }} />
           </div>
         </div>
         <div className="brand">
           <h1>Brand</h1>
           <FormGroup>
-            <FormControlLabel className="label" control={<Checkbox value="HP" onChange={brandCheckBoxHandler} />} label="HP" />
-            <FormControlLabel className="label" control={<Checkbox value="Asus" onChange={brandCheckBoxHandler} />} label="Asus" />
-            <FormControlLabel className="label" control={<Checkbox value="Dell" onChange={brandCheckBoxHandler} />} label="Dell" />
+            <FormControlLabel 
+              control={<Checkbox value="HP" onChange={brandCheckBoxHandler} />} 
+              label="HP" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="Asus" onChange={brandCheckBoxHandler} />} 
+              label="Asus" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="Dell" onChange={brandCheckBoxHandler} />} 
+              label="Dell" 
+            />
           </FormGroup>
         </div>
         <div className="procesor">
           <h1>Procesor</h1>
           <FormGroup>
-            <FormControlLabel control={<Checkbox value="ryzen5" onChange={procesorHandler} />} label="AMD Ryzen 5" />
-            <FormControlLabel control={<Checkbox value="i5" onChange={procesorHandler} />} label="Intel Core i5" />
-            <FormControlLabel control={<Checkbox value="ryzen7" onChange={procesorHandler} />} label="AMD Ryzen 7" />
-            <FormControlLabel control={<Checkbox value="i7" onChange={procesorHandler} />} label="Intel Core i7" />
+            <FormControlLabel 
+              control={<Checkbox value="ryzen5" onChange={procesorHandler} />} 
+              label="AMD Ryzen 5" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="i5" onChange={procesorHandler} />} 
+              label="Intel Core i5" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="ryzen7" onChange={procesorHandler} />} 
+              label="AMD Ryzen 7" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="i7" onChange={procesorHandler} />} 
+              label="Intel Core i7" 
+            />
           </FormGroup>
         </div>
         <div className="memory">
           <h1>Memory</h1>
           <FormGroup>
-            <FormControlLabel control={<Checkbox />} label="8 gb" />
-            <FormControlLabel control={<Checkbox />} label="16 gb" />
-            <FormControlLabel control={<Checkbox />} label="32 gb" />
+            <FormControlLabel 
+              control={<Checkbox value="8gb" onChange={memoryHandler}/>} 
+              label="8 gb" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="16gb" onChange={memoryHandler}/>} 
+              label="16 gb" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="32gb" onChange={memoryHandler}/>} 
+              label="32 gb" 
+            />
           </FormGroup>
         </div>
         <div className="drive">
           <h1>Drive</h1>
           <FormGroup>
-            <FormControlLabel control={<Checkbox />} label="1TB + 256GB SSD " />
-            <FormControlLabel control={<Checkbox />} label="1TB SSD" />
-            <FormControlLabel control={<Checkbox />} label="512GB SSD" />
+            <FormControlLabel 
+              control={<Checkbox value="1hdd+256ssd" onChange={driveHandler}/>} 
+              label="1TB + 256GB SSD " 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="1ssd" onChange={driveHandler}/>} 
+              label="1TB SSD" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="512ssd" onChange={driveHandler}/>} 
+              label="512GB SSD" 
+            />
           </FormGroup>
         </div>
         <div className="graphicsCard">
           <h1>Graphics Card</h1>
           <FormGroup>
-            <FormControlLabel control={<Checkbox />} label="GTX 1650" />
-            <FormControlLabel control={<Checkbox />} label="RTX 3050" />
-            <FormControlLabel control={<Checkbox />} label="RTX 3050 ti" />
+            <FormControlLabel 
+              control={<Checkbox value="gtx1650" onChange={graphicsCardHandler}/>} 
+              label="GTX 1650" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="rtx3050" onChange={graphicsCardHandler}/>} 
+              label="RTX 3050" 
+            />
+            <FormControlLabel 
+              control={<Checkbox value="rtx3050ti" onChange={graphicsCardHandler}/>} 
+              label="RTX 3050 ti" 
+            />
           </FormGroup>
         </div>
       </div>
