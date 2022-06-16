e8d00f37a0cba3b977f1045fe8aaa435ef065a6b
Dhia Ben Hammouda <dhiabenhamouda.5@gmail.com>
2022-06-16 13:29:34 +0200

    more work on auth

--- src/auth/SignIn.jsx
+++ src/auth/SignIn.jsx
@@ -7,18 +7,17 @@ import { FaTwitter } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
-const SignIn = ()=>{
-  const [email , setEmail ] = useState("");
-  const [password , setPassword ] = useState("");
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  async function submitHandler(e)
-  {
+  async function submitHandler(e) {
     e.preventDefault();
 
-    const response = await fetch("https://e-commerce-shop-react-js.herokuapp.com/auth/signIn",{
-      method:"POST",
-      header:{
-        "content-type":"application/json"
+    const response = await fetch("https://e-commerce-shop-react-js.herokuapp.com/auth/signIn", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
       },
       body: JSON.stringify({
         email,
@@ -26,36 +25,41 @@ const SignIn = ()=>{
       })
     });
 
-    const data = response.json();
-    console.log(data);
+    const data = await response.json();
+    
+    switch(data.msg)
+    {
+      
+    }
+
   }
 
-  return(
+  return (
     <>
       <div className="sign-in-container" onSubmit={submitHandler}>
         <form className="sign-in-form">
-          <IconContext.Provider value={{color:"#777" , size:"1.25rem"}}>
+          <IconContext.Provider value={{ color: "#777", size: "1.25rem" }}>
             <div className="input">
               <FaUser />
-              <input 
+              <input
                 type="email"
                 placeholder="Enter email..."
                 value={email}
-                onChange={(e)=>{setEmail(e.target.value)}}
+                onChange={(e) => { setEmail(e.target.value) }}
               />
             </div>
             <div className="input">
               <FaLock />
-              <input 
+              <input
                 type="password"
                 placeholder="Enter password..."
                 value={password}
-                onChange={(e)=>{setPassword(e.target.value)}}
+                onChange={(e) => { setPassword(e.target.value) }}
               />
             </div>
           </IconContext.Provider>
-          <div className="forgot" style={{margin:".5rem 0" ,width:"85%" ,display:"flex" , justifyContent:"flex-end" , color:"#777"}}>
-            <p style={{fontSize:"12px"}}>Forgot password ?</p>
+          <div className="forgot" style={{ margin: ".5rem 0", width: "85%", display: "flex", justifyContent: "flex-end", color: "#777" }}>
+            <p style={{ fontSize: "12px" }}>Forgot password ?</p>
           </div>
           <button type="submit">
             Sign in
@@ -66,16 +70,16 @@ const SignIn = ()=>{
           <div>
             <p className="sign-in-with">or sign in with</p>
           </div>
-          <div style={{display:"flex"}}>
-            <IconContext.Provider value={{size:"1.5rem" , color:"white"}}>
-              <div className="circle" style={{background:"#405498"}}>
-                <FaFacebookF/>
+          <div style={{ display: "flex" }}>
+            <IconContext.Provider value={{ size: "1.5rem", color: "white" }}>
+              <div className="circle" style={{ background: "#405498" }}>
+                <FaFacebookF />
               </div>
-              <div className="circle" style={{background:"#359BF1"}}>
-                <FaTwitter/>
+              <div className="circle" style={{ background: "#359BF1" }}>
+                <FaTwitter />
               </div>
-              <div className="circle" style={{background:"#E74639"}}>
-                <FaGoogle/>
+              <div className="circle" style={{ background: "#E74639" }}>
+                <FaGoogle />
               </div>
             </IconContext.Provider>
           </div>
--- src/auth/SignUp.jsx
+++ src/auth/SignUp.jsx
@@ -5,12 +5,15 @@ import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaSignature } from "react-icons/fa";
+import Alert from "@mui/material/Alert";
 
 const SignUp = ()=>{
   const [name , setName] = useState("");
   const [phone , setPhone] = useState("");
   const [email , setEmail ] = useState("");
   const [password , setPassword] = useState("");
+  const [success , setSuccess] = useState(false);
+  const [failure , setFailure] = useState(false);
 
   async function submitHandler(e)
   {
@@ -31,7 +34,14 @@ const SignUp = ()=>{
 
     const data = await response.json();
 
-    console.log(data);
+    if(data.msg === "user created succesfully")
+    {
+      setSuccess(true);
+    }
+    else if(data.msg === "User with the same email already exists")
+    {
+      setFailure(true);
+    }
   }
 
   return(
@@ -81,6 +91,16 @@ const SignUp = ()=>{
             <p className="already" style={{margin:".8rem 0" , color:"#777"}}> already have an account ? <a href="/signin" className="link">Sign in</a> </p>
           </div>
         </form>
+        {
+          success && <Alert className="alert" style={{position:"relative",top:"20px"}} variant="filled" color="success">
+          User created succesfully !
+        </Alert>
+        }
+        {
+          failure && <Alert className="alert" style={{position:"relative",top:"20px"}} variant="filled" color="error">
+          User with the same email already exists .
+        </Alert>
+        }
       </div>
     </>
   );
