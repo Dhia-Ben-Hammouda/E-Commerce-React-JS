00d2b3f2d09c54c4b8013add16df5e06d33d4d87
Dhia Ben Hammouda <dhiabenhamouda.5@gmail.com>
2022-06-18 00:00:51 +0200

    create cart layout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
 
-const Cart = ()=>{
-  return(
+const Cart = () => {
+  return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className="cart-container">
+        <div className="products">
+          <div className="product"></div>
+          <div className="product"></div>
+          <div className="product"></div>
+        </div>
+        <div className="checkout">
+          <div className="wrapper">
+            <h2>Total</h2>
+            <h3>7347.000 DT</h3>
+            <button>Checkout</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
