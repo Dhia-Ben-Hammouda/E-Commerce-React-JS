1f24a4b3767f26ca3e231893ef8f4f9cceb0b611
Dhia Ben Hammouda <dhiabenhamouda.5@gmail.com>
2022-07-07 23:06:37 +0200

    more work on ui

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -3,31 +3,30 @@ import Navbar from "./Navbar.jsx";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
-const AllProducts = ()=>{
+const AllProducts = () => {
   const { searchTerm } = useParams();
   const products = useSelector(state => state.search.products);
-  
+
+  console.log(searchTerm);
   console.log(products);
 
-  return(
+  return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="products-grid">
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
+        {
+          products.map((product, index) => {
+            return (
+              <div key={index} className="grid-item">
+                <div className="img-wrapper">
+                  <img src={product.picture} />
+                </div>
+                <h1>{product.name}</h1>
+                <h2>{product.description}</h2>
+              </div>
+            );
+          })
+        }
       </div>
     </>
   );
