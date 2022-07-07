db3becfe496b42b39850d7dabe50986ace95535b
Dhia Ben Hammouda <dhiabenhamouda.5@gmail.com>
2022-07-07 23:28:43 +0200

    more work on ui

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -15,14 +15,24 @@ const AllProducts = () => {
       <Navbar />
       <div className="products-grid">
         {
-          products.map((product, index) => {
+          products.filter((product)=>{
+            return product.name.toLowerCase().includes(searchTerm.toLowerCase())
+          }).map((product, index) => {
             return (
               <div key={index} className="grid-item">
                 <div className="img-wrapper">
                   <img src={product.picture} />
                 </div>
-                <h1>{product.name}</h1>
+                <h1>
+                  <a href={`/product/${product._id}`}>
+                    {product.name}
+                  </a>
+                </h1>
                 <h2>{product.description}</h2>
+                <div className="price-wrapper">
+                  <h3>{product.price} DT</h3>
+                  <button>Add To cart</button>
+                </div>
               </div>
             );
           })
